ff32175d31d3c1f0b542847713dc037d44a4d2a9
Emmanuel-Kokkinos <Emmanuel.Kokkinos@hotmail.com>
2022-07-11 15:05:49 -0400

    Initial Commit

---
+++ src/main/java/com/example/ExerciseHelper/ExerciseHelperApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExerciseHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ExerciseHelper/ExerciseHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExerciseHelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
